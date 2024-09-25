43a9aa6d0d15bc1d6ecb1658b649189d4446cf2d
Slaaaayz <maximec0109@example.com>
2024-09-25 23:35:46 +0200

    add value cookie

--- verify.php
+++ verify.php
@@ -4,7 +4,7 @@ $admin_id = "12345";
 $entered_id = $_POST['admin_id'];
 
 if ($entered_id === $admin_id) {
-    setcookie("admin_access", true, time() + 86400, "/");
+    setcookie("admin_access", "Frm38FALKVH#AAW", time() + 86400, "/");
     header("Location: admin_page.php");
     exit();
 } else {
