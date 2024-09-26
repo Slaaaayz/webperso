dd3166b6db342c32aae7ef78f175b54f8e06bdb0
Slaaaayz <maximec0109@example.com>
2024-09-26 14:05:54 +0200

    update 2 pages, add video and page admin

--- admin_page.php
+++ admin_page.php
@@ -6,14 +6,49 @@ if (isset($_COOKIE['admin_access']) != "Frm38FALKVH#AAW") {
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Admin</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Bienvenue sur la page Admin</h1>
-    <p>Ceci est une section protégée du site, accessible uniquement aux administrateurs.</p>
+    <div class="container">
+        <div class="header">
+            <a href="#">Acceuil</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Projets</a>
+            <a href="#">Clips</a>
+            <img src="./image/145207398.png">
+        </div>
+        <div class="select-game">
+            <div class="profile-game">
+                <a href="video.html"><img src="./image/r6.webp"></a>
+            </div>
+            <div class="profile-game">
+                <a href=""><img src="./image/valorant.jpeg"></a>
+            </div>
+            <div class="profile-game">
+                <a href=""><img src="./image/gtav.jpeg"></a>
+            </div>
+            <div class="profile-game">
+                <a href=""><img src="./image/codcw.jpeg"></a>
+            </div>
+            <div class="profile-game">
+                <a href=""><img src="./image/bodycam.jpeg"></a>
+            </div>
+            <div class="profile-game">
+                <a href=""><img src="./image/xdefiant.jpeg"></a>
+            </div>
+            <div class="profile-game">
+                <a href=""><img src="./image/desktop.jpg"></a>
+            </div>
+            <div class="profile-game">
+                <a href=""><img src="./image/fortnite.jpeg"></a>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
